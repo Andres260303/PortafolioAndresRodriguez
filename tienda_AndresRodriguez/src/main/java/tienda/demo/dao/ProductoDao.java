6e8c9a9a8cd4d8ee468f590c3b2c38f112e14d6b
Andres260303 <lrodriguez90310@ufide.ac.cr>
2024-03-20 18:09:57 -0600

    semana9

--- tienda_AndresRodriguez/src/main/java/tienda/demo/dao/ProductoDao.java
+++
@@ -1,9 +0,0 @@
-package tienda.demo.dao;
-
-import tienda.demo.domain.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoDao extends JpaRepository <Producto, Long> {
-    
-}
-
