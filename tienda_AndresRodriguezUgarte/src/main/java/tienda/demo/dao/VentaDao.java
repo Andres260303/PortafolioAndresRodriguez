89294d0b673d16883785f102846cc1e0e4e645c9
Andres260303 <lrodriguez90310@ufide.ac.cr>
2024-04-10 20:35:49 -0600

    Semana13
    Carrito

---
+++ tienda_AndresRodriguezUgarte/src/main/java/tienda/demo/dao/VentaDao.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package tienda.demo.dao;
+
+/**
+ *
+ * @author ulgcu
+ */
+import tienda.demo.domain.Venta;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface VentaDao extends JpaRepository <Venta,Long>
+{
+}
